3280fdf13c2f10e9023be11f016488fb524a9b2e
Arfan Siregar <arfansiregar.33@gmail.com>
2024-02-28 20:24:47 +0700

    feat/ARL-001: Adding Kdoc

--- src/main/kotlin/exercise/HelloWorld.kt
+++ src/main/kotlin/exercise/HelloWorld.kt
@@ -1,5 +1,11 @@
 package exercise
 
+/**
+ * @author: Arfan Siregar
+ *
+ * Creating exercise for displaying hello world
+ * */
+
 fun main() {
     println("Hello World!")
     print("Hello World!")
